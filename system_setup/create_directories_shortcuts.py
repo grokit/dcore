d0b18a121419e45552f968cb2f4c0c33296e072c
david <reachme.web@outlook.com>
2014-06-01 14:55:01 -0700

    ++

--- system_setup/create_directories_shortcuts.py
+++ system_setup/create_directories_shortcuts.py
@@ -13,27 +13,45 @@ On Linux, you need to prepend '. ':
 import dcore.system_description as sd
 
 import os
+import sys
+import argparse
+
+_meta_shell_command = 'cdgen'
 
 if __name__ == '__main__':
+ 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l', '--list', action = "store_true")
+    parser.add_argument('-g', '--gen',  action = "store_true")
+
+    args = parser.parse_args()
         
     dirsMap = sd.getDirsMap()
-    
-    file_ext, output_dir, file_template = sd.getPythonScriptsEnv()
-    
-    fileContentTemplate = sd.getAutogenFileTemplate()
-    
-    # Generate all folder shortcuts with 'cd' prefix.
-    # So if a folder has the shortcut 'app', you can access it on the console by doing 'cdapp'
-    for shortcut, folder in dirsMap.items():
+ 
+    if args.list == True:
+        for k, v in dirsMap.items():
+            print("%-15s: %s" % (k, v))
+        sys.exit(0)
+    elif args.gen == True: 
+        file_ext, output_dir, file_template = sd.getPythonScriptsEnv()
         
-        fileOut = output_dir + "/" + 'cd' + shortcut + file_ext
-        fileContent = fileContentTemplate.replace('__custom__', 'cd %s' % folder)
+        fileContentTemplate = sd.getAutogenFileTemplate()
         
-        fh = open(fileOut, 'w')
-        fh.write(fileContent)
-        fh.close()
-        
-        print('Created shortcut %s.' % fileOut)
-        
-        if os.name == 'posix':
-            os.system('chmod +x %s' % fileOut)
+        # Generate all folder shortcuts with 'cd' prefix.
+        # So if a folder has the shortcut 'app', you can access it on the console by doing 'cdapp'
+        for shortcut, folder in dirsMap.items():
+            
+            fileOut = output_dir + "/" + 'cd' + shortcut + file_ext
+            fileContent = fileContentTemplate.replace('__custom__', 'cd %s' % folder)
+            
+            fh = open(fileOut, 'w')
+            fh.write(fileContent)
+            fh.close()
+            
+            print('Created shortcut %s.' % fileOut)
+            
+            if os.name == 'posix':
+                os.system('chmod +x %s' % fileOut)
+    else:
+        raise Exception("Invalid script arguments: %s." % args)
