27a22efdd546e3b7cb6f3d0c3549994c793979a8
david <reachme.web@outlook.com>
2014-06-03 00:20:01 -0700

    javarun

---
+++ shell_ext/javarun.py
@@ -0,0 +1,33 @@
+
+_meta_shell_command = 'javarun'
+
+import os
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-f', '--file', type=str, default = None)
+    args = parser.parse_args()
+    
+    filec = []
+    if args.file != None:
+        filesc = [args.file]
+    else:
+        files = os.listdir('.')
+        filesc = [file for file in files if file[-4:].lower() == 'java']
+
+    rv = 0;
+    for file in filesc:
+        cmd = 'javac %s' % file
+        print(cmd)
+        rv |= os.system(cmd)
+
+    if rv == 0:
+        os.system('echo begin > out.out')
+        for file in [f.replace('.class', '') for f in os.listdir('.') if '.class' in f]:
+            cmd = 'java %s >> out.out' % file
+            print(cmd)
+            os.system(cmd)
+            os.system('cat out.out')
+
