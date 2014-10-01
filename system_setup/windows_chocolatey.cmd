d3b6e9fe2877170eb0b083f47ad7ade69411d222
grokit <grokit@users.noreply.github.com>
2014-10-01 13:14:45 -0400

    Update windows_chocolatey.cmd

--- system_setup/windows_chocolatey.cmd
+++ system_setup/windows_chocolatey.cmd
@@ -1,54 +1,63 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
+rem # As
+
 choco install git
-choco install Graphviz
 choco install notepadplusplus.install
-choco install 7zip
 choco install 7zip.commandline
-choco install vlc
 choco install python
-choco install putty
-choco install filezilla
-choco install Console2
-choco install skype
-choco install meld
-choco install fiddler
-choco install virtualbox
-choco install vim
 choco install Wget
-choco install windirstat
 choco install curl
 choco install autohotkey.portable
-choco install calibre
 choco install hg
-choco install PDFCreator
-choco install ConEmu
-choco install gimp
+choco install putty
 choco install winmerge
-choco install greenshot
-choco install wireshark
-choco install audacity
-choco install screenpresso
-choco install Cygwin
-choco install winscp
-choco install youtube-dl
+choco install vim
+
+rem # Bs
+
+choco install Console2
 choco install launchy
-choco install ConsoleZ
-choco install kdiff3
+choco install greenshot
+choco install PDFCreator
+choco install Cygwin
+choco install youtube-dl
 choco install foobar2000
-choco install expresso
+choco install gimp
+choco install vlc
 choco install imagemagick.app
+choco install winscp
 choco install PDFXChangeViewer
+choco install ffmpeg
+choco install ditto
+choco install windowsessentials
+choco install windirstat
+
+rem # Cs
+
+choco install calibre
+choco install Graphviz
+choco install ConEmu
+choco install wireshark
+choco install kdiff3
 choco install nmap
 choco install InkScape
-choco install rdcman
 choco install truecrypt
 choco install ilspy
-choco install ffmpeg
 choco install windbg
 choco install dependencywalker
 choco install blender
 choco install procmon
 choco install ilmerge
-choco install ditto
-choco install windowsessentials
+choco install filezilla
+choco install skype
+choco install meld
+choco install fiddler
+
+rem # Ds
+rem choco install screenpresso
+rem choco install virtualbox
+rem choco install audacity
+rem choco install expresso
+rem choco install ConsoleZ
+rem choco install rdcman
