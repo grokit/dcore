a160355af288fc3c2c0c2f003035e7213a4b79c6
david <reachme.web@outlook.com>
2014-11-01 01:18:40 -0700

    retired

---
+++ shell_ext/README.md
@@ -0,0 +1,9 @@
+
+# Shell Extentions
+
+The scripts in this directory automatically generate shortcuts in Windows and Linux that invoke them from anywhere in a shell.
+
+# Ideas
+
+Maybe automatically name the shortcut the same as the file without the .py?
+- Could have an option for an additional alias (creates two shortcuts)?
---
+++ shell_ext/reminder.py
@@ -0,0 +1,26 @@
+﻿
+import argparse
+import time
+
+import dcore.system_description as private_data
+import dcore.shell_ext.gmail as gmail
+
+_meta_shell_command = 'reminder'
+
+def getArgs():
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('subject', nargs='+')
+    parser.add_argument('-t', '--to', default= private_data.primary_email)
+    parser.add_argument('-b', '--body', default='')
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    
+    args = getArgs()
+    print(args)
+    
+    gmail.do(args.to, " ".join(args.subject), args.body)
