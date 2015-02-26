a8688353144f4126bb68054a6d7c79188e29a603
david <reachme.web@outlook.com>
2015-02-25 23:25:29 -0800

    ++

--- shell_ext/scalarun.py
+++ shell_ext/scalarun.py
@@ -11,26 +11,37 @@ def isSourceFile(filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', type=str, nargs='?', default = None)
+    parser.add_argument('file_and_feed', type=str, nargs='?', default = None)
     args = parser.parse_args()
 
-    filec = []
-    if args.file != None:
-        filesc = [args.file]
-    else:
-        files = os.listdir('.')
-        filesc = [file for file in files if isSourceFile(file)]
+    filesc = [file for file in os.listdir('.') if isSourceFile(file)]
+    assert len(filesc) == 1
+    filesc = filesc[0]
 
-    rv = 0;
-    for file in filesc:
-        os.system('mkdir out')
-        cmd = 'scalac %s -d out' % (file)
-        print(cmd)
-        rv |= os.system(cmd)
+    feed = None
+    if args.file_and_feed is not None:
+        feed = args.file_and_feed
+    print('feed', feed)
+
+    rv = 0
+    os.system('mkdir out')
+    cmd = 'scalac %s -d out' % (filesc)
+    print(cmd)
+    rv |= os.system(cmd)
 
     if rv == 0:
-        classFile = [os.path.splitext(x)[0] for x in os.listdir('./out') if os.path.splitext(x)[1] == '.class'][0]
-        cmd = 'scala -cp ./out/ ' + classFile + ' > ./out/' +classFile  + '.out'
+        classFile = [os.path.splitext(x)[0] for x in os.listdir('./out') if os.path.splitext(x)[1] == '.class' and x.find('$') == -1]
+
+        if len(classFile) != 1:
+            print("Don't know what to run from: %s." % classFile)
+            exit(-1)
+        classFile = classFile[0]
+
+        feedStr = ''
+        if feed is not None:
+            feedStr = ' < %s ' % feed
+        cmd = 'scala -cp ./out/ ' + classFile + feedStr + ' > ./out/' +classFile  + '.out'
         print(cmd)
         os.system(cmd)
         os.system('cat ./out/' + classFile + '.out')
+
