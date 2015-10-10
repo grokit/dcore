c5312bc573733024a0ed01f9898628485b8a89b5
arch2 <reachme.web@outlook.com>
2015-10-10 15:32:53 -0700

    fix render

--- apps/task_track_v2/html_render.py
+++ apps/task_track_v2/html_render.py
@@ -2,19 +2,19 @@
 http://d3js.org/ ??
 """
 
-import work_unit
-
 import os
 
 import options
+import work_unit
+import serialization_json  
 
 def render(wd, htmlFilename):
 	template = open(os.path.join(options.mainDir, 'template.html')).read()
 	open(htmlFilename, 'w').write(template)
 
 def unitTests():
-	filename = os.path.join(options.utFolder, 'unit-tests-%s.html' % __file__)
-	wd = work_unit.createTestData()
+	filename = os.path.join(options.utFolder, 'unit-tests-render_html.html')
+	wd = serialization_json.createTestData()
 	render(wd, filename)
 
 if __name__ == '__main__':
