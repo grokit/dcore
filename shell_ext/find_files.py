8784a2f7cd4e5ed21c4b637c16bdf6c5ab74a9c0
arch2 <reachme.web@outlook.com>
2015-10-11 14:09:11 -0700

    find files: add cache of 12 hours

--- shell_ext/find_files.py
+++ shell_ext/find_files.py
@@ -2,13 +2,15 @@
 import os
 import argparse
 import pickle
+import datetime
 
 _meta_shell_command = 'ff'
 
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('grep', type=str, nargs='?', default = None)
-    parser.add_argument('-f', '--filenames_only', action="store_true")
+    # NOT CODED:
+    #parser.add_argument('-f', '--filenames_only', action="store_true")
     args = parser.parse_args()
     return args
 
@@ -31,11 +33,47 @@ def filterOutIfArrayInElement(F, filterArray):
 def filterInCaseInsensitive(F, astr):
     return [f for f in F if astr in f.lower()]
 
+def dateNow():
+    return datetime.datetime.now(datetime.timezone.utc)
+
+def dateNowStr():
+    return dateNow().isoformat()
+
+class Cache:
+    def __init__(self, F):
+        self.F = F
+        self.date = dateNow() 
+
+def gen():
+    # root = '~/sync/dev/Dropbox/scripts/dcore/shell_ext'
+    root = '~/sync'
+    F = getAllFiles(os.path.expanduser(root))
+    F = filterOutIfArrayInElement(F, ['.git', '__pycache__'])
+    return F
+
 def do():
     args = getArgs()
 
-    F = getAllFiles(os.path.expanduser('~/sync'))
-    F = filterOutIfArrayInElement(F, ['.git', '__pycache__'])
+    cache = None
+    cacheLoc = os.path.expanduser('~/sync/ff_cache.pickle')
+
+    F = None
+    if os.path.isfile(cacheLoc):
+        print('Loading cache from: %s.', cacheLoc)
+        cache = pickle.load(open(cacheLoc, 'rb'))
+        cacheAge = dateNow() - cache.date
+        print('Cache age = %s.' % cacheAge)
+        if cacheAge.total_seconds() < 12*(60*60):
+            F = cache.F
+        else:
+            print('Cache too old, wiping.')
+
+    if F is None:
+        print('Generating cache... this could take a while... grab a coffee and relax :).')
+        F = gen()
+        print('Saving cache at: %s.' % cacheLoc)
+        pickle.dump(Cache(F), open(cacheLoc, 'wb'))
+
 
     if args.grep is not None:
         args.grep = args.grep.lower()
