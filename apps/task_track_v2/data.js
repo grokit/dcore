1cfa32d8a084327bc24543605e8c617f49ba752f
arch2 <reachme.web@outlook.com>
2015-10-19 23:01:10 -0700

    display something

--- apps/task_track_v2/data.js
+++ apps/task_track_v2/data.js
@@ -1,2 +1,129 @@
-
-var data = [ { taskDuration: 100.2, type: "cs", date: 1111 }, { taskDuration: 300, type: "medi", date: 1121 } ];
+var data =
+[
+    {
+        date: 1444009979.0,
+        type: "medi",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1444016207.0,
+        type: "cs",
+        length: 1.25,
+        comment:"anon"
+    },
+    {
+        date: 1444110126.0,
+        type: "cs",
+        length: 1.6,
+        comment:"anon"
+    },
+    {
+        date: 1444145562.0,
+        type: "weight",
+        length: 0.3,
+        comment:"anon"
+    },
+    {
+        date: 1444184420.0,
+        type: "exer",
+        length: 0.7,
+        comment:"anon"
+    },
+    {
+        date: 1444208510.0,
+        type: "cs",
+        length: 0.8,
+        comment:"anon"
+    },
+    {
+        date: 1444286181.0,
+        type: "cs",
+        length: 1.25,
+        comment:"anon"
+    },
+    {
+        date: 1444356370.0,
+        type: "weight",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1444528870.0,
+        type: "code",
+        length: 3.5,
+        comment:"anon"
+    },
+    {
+        date: 1444530008.0,
+        type: "cs",
+        length: 1.0,
+        comment:"anon"
+    },
+    {
+        date: 1444608050.0,
+        type: "publish",
+        length: 1.0,
+        comment:"anon"
+    },
+    {
+        date: 1444610290.0,
+        type: "cs",
+        length: 2.5,
+        comment:"anon"
+    },
+    {
+        date: 1444611523.0,
+        type: "cs",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1444706081.0,
+        type: "weight",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1444716698.0,
+        type: "code",
+        length: 1.5,
+        comment:"anon"
+    },
+    {
+        date: 1444718014.0,
+        type: "cs",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1445148307.0,
+        type: "cs",
+        length: 8.0,
+        comment:"anon"
+    },
+    {
+        date: 1445154749.0,
+        type: "cs",
+        length: 1.5,
+        comment:"anon"
+    },
+    {
+        date: 1445156315.0,
+        type: "cs",
+        length: 0.5,
+        comment:"anon"
+    },
+    {
+        date: 1445194508.0,
+        type: "cs",
+        length: 2.0,
+        comment:"anon"
+    },
+    {
+        date: 1445313221.0,
+        type: "exer",
+        length: 0.3,
+        comment:"anon"
+    }
+]
--- apps/task_track_v2/html_render.py
+++ apps/task_track_v2/html_render.py
@@ -4,6 +4,10 @@ Excellent tutorial here: http://alignedleft.com/tutorials/d3.
 """
 
 import os
+import json
+import copy
+import time
+import re
 
 import options
 import work_unit
@@ -12,6 +16,29 @@ import serialization_json
 def render(wd, htmlFilename):
 	template = open(os.path.join(options.mainDir, 'template.html')).read()
 	open(htmlFilename, 'w').write(template)
+
+	workUnits = serialization_json.fromFile(options.dbFile)
+
+	def dateToUnixTime(w):
+		ww = copy.deepcopy(w)
+		dd = time.mktime(ww.date.timetuple())
+		print(type(dd))
+		ww.date = dd
+		return ww
+
+	workUnits = list(map( dateToUnixTime, workUnits))
+
+	filenameOut = 'data.js'
+	ser = json.dumps(workUnits, cls=serialization_json.Encoder, indent=4)
+	# Need to dump as javascript object...
+	lines = ser.split('\n')
+	out = ['var data =']
+	for l in lines:
+		l = re.sub(r'"(\w+)":', r'\1:', l)
+		l = re.sub(r'comment.*?".*?"', r'comment:"anon"', l)
+		out.append(l)
+
+	open(filenameOut, 'w').write("\n".join(out))
 
 def unitTests():
 	filename = os.path.join(options.utFolder, 'unit-tests-render_html.html')
