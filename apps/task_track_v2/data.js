e754bcccb5d28ff0922405ccbdcbec24f3b6c277
arch2 <reachme.web@outlook.com>
2015-10-19 23:28:54 -0700

    better

--- apps/task_track_v2/data.js
+++ apps/task_track_v2/data.js
@@ -1,129 +1,135 @@
 var data =
 [
     {
-        date: 1444009979.0,
+        length: 0.5,
+        comment:"anon",
         type: "medi",
-        length: 0.5,
-        comment:"anon"
+        date: 1444009979.0
     },
     {
-        date: 1444016207.0,
-        type: "cs",
         length: 1.25,
-        comment:"anon"
+        comment:"anon",
+        type: "cs",
+        date: 1444016207.0
     },
     {
-        date: 1444110126.0,
-        type: "cs",
         length: 1.6,
-        comment:"anon"
+        comment:"anon",
+        type: "cs",
+        date: 1444110126.0
     },
     {
-        date: 1444145562.0,
-        type: "weight",
         length: 0.3,
-        comment:"anon"
+        comment:"anon",
+        type: "weight",
+        date: 1444145562.0
     },
     {
-        date: 1444184420.0,
-        type: "exer",
         length: 0.7,
-        comment:"anon"
-    },
-    {
-        date: 1444208510.0,
-        type: "cs",
-        length: 0.8,
-        comment:"anon"
-    },
-    {
-        date: 1444286181.0,
-        type: "cs",
-        length: 1.25,
-        comment:"anon"
-    },
-    {
-        date: 1444356370.0,
-        type: "weight",
-        length: 0.5,
-        comment:"anon"
-    },
-    {
-        date: 1444528870.0,
-        type: "code",
-        length: 3.5,
-        comment:"anon"
-    },
-    {
-        date: 1444530008.0,
-        type: "cs",
-        length: 1.0,
-        comment:"anon"
-    },
-    {
-        date: 1444608050.0,
-        type: "publish",
-        length: 1.0,
-        comment:"anon"
-    },
-    {
-        date: 1444610290.0,
-        type: "cs",
-        length: 2.5,
-        comment:"anon"
-    },
-    {
-        date: 1444611523.0,
-        type: "cs",
-        length: 0.5,
-        comment:"anon"
-    },
-    {
-        date: 1444706081.0,
-        type: "weight",
-        length: 0.5,
-        comment:"anon"
-    },
-    {
-        date: 1444716698.0,
-        type: "code",
-        length: 1.5,
-        comment:"anon"
-    },
-    {
-        date: 1444718014.0,
-        type: "cs",
-        length: 0.5,
-        comment:"anon"
-    },
-    {
-        date: 1445148307.0,
-        type: "cs",
-        length: 8.0,
-        comment:"anon"
-    },
-    {
-        date: 1445154749.0,
-        type: "cs",
-        length: 1.5,
-        comment:"anon"
-    },
-    {
-        date: 1445156315.0,
-        type: "cs",
-        length: 0.5,
-        comment:"anon"
-    },
-    {
-        date: 1445194508.0,
-        type: "cs",
-        length: 2.0,
-        comment:"anon"
-    },
-    {
-        date: 1445313221.0,
+        comment:"anon",
         type: "exer",
+        date: 1444184420.0
+    },
+    {
+        length: 0.8,
+        comment:"anon",
+        type: "cs",
+        date: 1444208510.0
+    },
+    {
+        length: 1.25,
+        comment:"anon",
+        type: "cs",
+        date: 1444286181.0
+    },
+    {
+        length: 0.5,
+        comment:"anon",
+        type: "weight",
+        date: 1444356370.0
+    },
+    {
+        length: 3.5,
+        comment:"anon",
+        type: "code",
+        date: 1444528870.0
+    },
+    {
+        length: 1.0,
+        comment:"anon",
+        type: "cs",
+        date: 1444530008.0
+    },
+    {
+        length: 1.0,
+        comment:"anon",
+        type: "publish",
+        date: 1444608050.0
+    },
+    {
+        length: 2.5,
+        comment:"anon",
+        type: "cs",
+        date: 1444610290.0
+    },
+    {
+        length: 0.5,
+        comment:"anon",
+        type: "cs",
+        date: 1444611523.0
+    },
+    {
+        length: 0.5,
+        comment:"anon",
+        type: "weight",
+        date: 1444706081.0
+    },
+    {
+        length: 1.5,
+        comment:"anon",
+        type: "code",
+        date: 1444716698.0
+    },
+    {
+        length: 0.5,
+        comment:"anon",
+        type: "cs",
+        date: 1444718014.0
+    },
+    {
+        length: 8.0,
+        comment:"anon",
+        type: "cs",
+        date: 1445148307.0
+    },
+    {
+        length: 1.5,
+        comment:"anon",
+        type: "cs",
+        date: 1445154749.0
+    },
+    {
+        length: 0.5,
+        comment:"anon",
+        type: "cs",
+        date: 1445156315.0
+    },
+    {
+        length: 2.0,
+        comment:"anon",
+        type: "cs",
+        date: 1445194508.0
+    },
+    {
         length: 0.3,
-        comment:"anon"
+        comment:"anon",
+        type: "exer",
+        date: 1445313221.0
+    },
+    {
+        length: 1.0,
+        comment:"anon",
+        type: "code",
+        date: 1445322222.0
     }
 ]
