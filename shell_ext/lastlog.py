c0ad5970b480001d0714c61f9dda3240e8137a40
arch2 <reachme.web@outlook.com>
2015-11-01 23:13:38 -0800

    ll

---
+++ shell_ext/lastlog.py
@@ -0,0 +1,20 @@
+﻿"""
+"""
+
+_meta_shell_command = 'll'
+
+import os
+import argparse
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = getArgs()
+    print(args)
+    files = [f for f in os.listdir('.') if "log" in f.lower()]
+    file = max(files, key=os.path.getctime)
+    print(file)
+    cmd = 'np %s' % file
+    os.system(cmd)
