8a0853492cad57adceb3062dd91e19bc8011c0b0
arch2 <reachme.web@outlook.com>
2015-11-05 22:48:32 -0800

    looks good

--- apps/task_track_v2/data.js
+++ apps/task_track_v2/data.js
@@ -1,147 +1,177 @@
 var data =
 [
     {
+        comment:"anon",
         date: 1444009979.0,
         type: "medi",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1444016207.0,
         type: "cs",
-        comment:"anon",
         length: 1.25
     },
     {
+        comment:"anon",
         date: 1444110126.0,
         type: "cs",
-        comment:"anon",
         length: 1.6
     },
     {
+        comment:"anon",
         date: 1444145562.0,
         type: "weight",
-        comment:"anon",
         length: 0.3
     },
     {
+        comment:"anon",
         date: 1444184420.0,
         type: "exer",
-        comment:"anon",
         length: 0.7
     },
     {
+        comment:"anon",
         date: 1444208510.0,
         type: "cs",
-        comment:"anon",
         length: 0.8
     },
     {
+        comment:"anon",
         date: 1444286181.0,
         type: "cs",
-        comment:"anon",
         length: 1.25
     },
     {
+        comment:"anon",
         date: 1444356370.0,
         type: "weight",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1444528870.0,
         type: "code",
-        comment:"anon",
         length: 3.5
     },
     {
+        comment:"anon",
         date: 1444530008.0,
         type: "cs",
-        comment:"anon",
         length: 1.0
     },
     {
+        comment:"anon",
         date: 1444608050.0,
         type: "publish",
-        comment:"anon",
         length: 1.0
     },
     {
+        comment:"anon",
         date: 1444610290.0,
         type: "cs",
-        comment:"anon",
         length: 2.5
     },
     {
+        comment:"anon",
         date: 1444611523.0,
         type: "cs",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1444706081.0,
         type: "weight",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1444716698.0,
         type: "code",
-        comment:"anon",
         length: 1.5
     },
     {
+        comment:"anon",
         date: 1444718014.0,
         type: "cs",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1445148307.0,
         type: "cs",
-        comment:"anon",
         length: 8.0
     },
     {
+        comment:"anon",
         date: 1445154749.0,
         type: "cs",
-        comment:"anon",
         length: 1.5
     },
     {
+        comment:"anon",
         date: 1445156315.0,
         type: "cs",
-        comment:"anon",
         length: 0.5
     },
     {
+        comment:"anon",
         date: 1445194508.0,
         type: "cs",
-        comment:"anon",
         length: 2.0
     },
     {
+        comment:"anon",
         date: 1445313221.0,
         type: "exer",
-        comment:"anon",
         length: 0.3
     },
     {
+        comment:"anon",
         date: 1445322222.0,
         type: "code",
-        comment:"anon",
         length: 1.0
     },
     {
+        comment:"anon",
         date: 1445580501.0,
         type: "cs",
-        comment:"anon",
         length: 2.0
     },
     {
+        comment:"anon",
         date: 1445580515.0,
         type: "exer",
-        comment:"anon",
         length: 0.5
+    },
+    {
+        comment:"anon",
+        date: 1445796874.0,
+        type: "cs",
+        length: 0.75
+    },
+    {
+        comment:"anon",
+        date: 1445822709.0,
+        type: "cs",
+        length: 3.5
+    },
+    {
+        comment:"anon",
+        date: 1446316156.0,
+        type: "cs",
+        length: 2.5
+    },
+    {
+        comment:"anon",
+        date: 1446538011.0,
+        type: "cs",
+        length: 1.0
+    },
+    {
+        comment:"anon",
+        date: 1446706538.0,
+        type: "code",
+        length: 1.0
     }
 ]
