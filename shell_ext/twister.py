28e4524b65bce1b0b398f71f7b693831f2053dee
arch <arch@a.com>
2016-04-17 10:50:50 -0700

    gitpp: auto commit

--- shell_ext/twister.py
+++ shell_ext/twister.py
@@ -10,27 +10,42 @@ _meta_shell_command = 'twister'
 
 def getArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument('src', nargs='+')
+    parser.add_argument('-s', '--secret', default=None, help='Value to hash (if do not want to enter in prompt). Careful as this will show in your bash history.')
+    parser.add_argument('-f', '--file', default=None, help='Filename to get secret from. Will only get first line and remove endline characters.')
     args = parser.parse_args()
     return args
 
-scriptSalt = '7vuv4a$yjsv7Tm)ubku4q4omodkv^f0dr6rv0kW5zssd1k9g(Mnkifcgpi1easj5v7o$cflsy6m>JLpm3byxbcuBmglw5k7@i6Logri2*rcjc8gh77-j00z;61k75xew'
-scriptVersion = 2
-
-def hash(v):
+def hash20160417(v):
     """
     v: value to `twist`.
     return: twisted value as hex-string representation of binary result.
     """
 
+    scriptSalt = '7vuv4a$yjsv7Tm)ubku4q4omodkv^f0dr6rv0kW5zssd1k9g(Mnkifcgpi1easj5v7o$cflsy6m>JLpm3byxbcuBmglw5k7@i6Logri2*rcjc8gh77-j00z;61k75xew'
     secretSalt = private_data.sk0
-    salt = "%s-%s-%s" % (scriptSalt, secretSalt, scriptVersion)
+    salt = "%s-%s" % (scriptSalt, secretSalt)
+
     dk = hashlib.pbkdf2_hmac('sha256', v.encode(), salt.encode(), 1000000)
     return binascii.hexlify(dk).decode()
 
+# Just point to latest hash function. ALWAYS keep past hash functions in case need to recover an old hash.
+hash = hash20160417
+
 if __name__ == '__main__':
     args = getArgs()
-    lkey = " ".join(args.src)
-    pw = getpass.getpass(prompt='Enter key:\n')
-    v = hash("%s-%s" % (lkey, pw))
+
+    if args.secret is not None:
+        secret = args.secret
+    elif args.file is not None:
+        secret = open(args.file).readlines()
+        if len(secret) != 1:
+            raise Exception('Secret file has more than one line.')
+        secret = secret[0].strip()
+    else:
+        secret = getpass.getpass(prompt='Enter secret:\n')
+
+    #print('Secret: `%s`.' % secret)
+
+    v = hash(secret)
     print(v)
+
