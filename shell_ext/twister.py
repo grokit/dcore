7211cb5090d19eeebf628300d31ced7077847e04
arch <arch@a.com>
2016-04-17 10:56:25 -0700

    gitpp: auto commit

--- shell_ext/twister.py
+++ shell_ext/twister.py
@@ -21,7 +21,7 @@ def hash20160417(v):
     return: twisted value as hex-string representation of binary result.
     """
 
-    scriptSalt = '7vuv4a$yjsv7Tm)ubku4q4omodkv^f0dr6rv0kW5zssd1k9g(Mnkifcgpi1easj5v7o$cflsy6m>JLpm3byxbcuBmglw5k7@i6Logri2*rcjc8gh77-j00z;61k75xew'
+    scriptSalt = '7vuv4a$yj"vTm)u4omdk^f0rk5\sd9(MiGe7o$cfsym>JLpb%bBgwk7@i6L/ogri2*rcj8h77-j00z;61k75xew'
     secretSalt = private_data.sk0
     salt = "%s-%s" % (scriptSalt, secretSalt)
 
