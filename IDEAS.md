4b910213dce22043b01388216238de2ebc873022
arch <arch@a.com>
2016-09-05 21:13:04 -0700

    gitpp: auto commit

---
+++ IDEAS.md
@@ -0,0 +1,5 @@
+
+# Test-Edit File with Content
+
+file content
+--> open in vim any file that has "file content" in. If > 1 ask for selection.
---
+++ shell_ext/edit_script.py
@@ -0,0 +1,55 @@
+
+import dcore.search_files as fsearch
+import dcore.system_setup.create_python_scripts_shortcuts as scripts_info
+import dcore.data as data
+
+import argparse
+import os
+
+_meta_shell_command = 'edits'
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('shortcut_to_edit')
+    args = parser.parse_args()
+    return args
+
+def findExecutableScripts():
+    pyPublic = data.dcoreRoot()
+    pyfiles = fsearch.getAllFilesRecursively('*.py', pyPublic)
+
+    tag = scripts_info.shell_meta_search
+
+    of = []
+    for pfile in pyfiles:
+        lines = open(pfile).readlines()
+
+        for l in lines:
+            if tag in l:
+                stag = l.split('=')[1].strip()
+                if len(stag) > 1: stag = stag.split(' ')[0]
+                stag = stag.strip('"')
+                stag = stag.strip("'")
+                of.append((pfile,stag))
+                break
+
+    return of 
+
+if __name__ == '__main__':
+    args = getArgs()
+
+    editor = 'vim'
+
+    efiles = findExecutableScripts()
+
+    filename = []
+    for f, tag in efiles:
+        if tag == args.shortcut_to_edit:
+            filename.append(f)
+
+    assert len(filename) == 1
+    filename = filename[0]
+    cmd = 'vim %s' % filename
+    print(cmd)
+    os.system(cmd)
+
