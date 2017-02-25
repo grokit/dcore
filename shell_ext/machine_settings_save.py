04837436b3775f8ce8e0793727f83770f6b13af1
arch <arch@a.com>
2017-02-25 13:42:53 -0800

    gitpp: auto commit

---
+++ shell_ext/machine_settings_save.py
@@ -0,0 +1,12 @@
+
+
+# Can be eiter file or folder, backup will act accordingly.
+TO_SAVE = [
+'~/.gdbinit',
+'~/.inputrc',
+'~/.bashrc',
+'~/.bash_history',
+'~/.tmux.conf',
+'~/.vimrc',
+'~/.vim',
+]
