55c6c404d49b1c7a9e551321b4eb5efbce44e6a2
arch <arch@a.com>
2017-11-05 16:47:48 -0800

    gitpp: auto commit

--- shell_ext/rename_all.py
+++ shell_ext/rename_all.py
@@ -2,6 +2,7 @@
 
 """
 Rename filenames and file content.
+Applies recursively on all files by default.
 """
 
 import re
@@ -11,17 +12,24 @@ import time
 
 _meta_shell_command = 'rename_all'
 
+DEBUG = True
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('f', help='from')
+    parser.add_argument('t', help='to')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--nocontent', action='store_true')
+    parser.add_argument('--nofilename', action='store_true')
+    args = parser.parse_args()
+    return args
+
 def walkGatherAllFiles(rootdir = '.'):
     F = []
     for dirpath, dirnames, filenames in os.walk(rootdir):
         for f in filenames:
             F.append( os.path.join(dirpath, f) )
     return F
-
-def getArgs():
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    return args
 
 def containsHiddenFolder(filename):
     pre, fn = os.path.split(os.path.abspath(filename))
@@ -30,10 +38,48 @@ def containsHiddenFolder(filename):
         if len(p) > 1 and p[0] == '.': return True
     return False
 
-if __name__ == '__main__':
-    files = walkGatherAllFiles('.')
-    files = [f for f in files if not containsHiddenFolder(f)]
+def renameFileContent(filename, frm, to):
+    if DEBUG:
+        print('renameFileContent ', filename)
+    with open(filename, 'r') as fh:
+        content = fh.read()
+    with open(filename, 'w') as fh:
+        content = content.replace(frm, to)
+        fh.write(content)
 
+def renameFilename(filename, frm, to):
+    pre, fn = os.path.split(os.path.abspath(filename))
+    fn = fn.replace(frm, to)
+    fn = os.path.join(pre,fn)
+    if DEBUG:
+        print('renameFilename ', filename, fn)
+    os.rename(filename, fn)
+
+def getFiles():
+    files = walkGatherAllFiles('.')
+
+    F = []
     for f in files:
-        print(f)
+        if not containsHiddenFolder(f):
+            F.append(f)
+        else:
+            if DEBUG:
+                pass
+                #print('Skipping ', f)
+    return F
+
+if __name__ == '__main__':
+    args = getArgs()
+    print(args)
+    DEBUG = args.debug
+
+    if not args.nocontent:
+        for f in getFiles():
+            renameFileContent(f, args.f, args.t)
+
+    if not args.nofilename:
+        for f in getFiles():
+            renameFilename(f, args.f, args.t)
+
+
 
