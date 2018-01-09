1113981dd9a9f9e5bfd1035fa92a5d51a5dcbead
d-asus <you@example.com>
2018-01-08 23:47:53 -0800

    gitpp: auto commit

--- shell_ext/backup_remote.py
+++ shell_ext/backup_remote.py
@@ -25,6 +25,14 @@ def getArgs():
 def getBackupPWAndUrl():
     return private_data.k_remote_backup_backblaze_v1, private_data.backblaze_b2_url
 
+def executeCmd(cmd):
+    cmd = cmd.split(' ')
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for l in p.stdout:
+        yield l
+    p.stdout.close()
+    return p.wait()
+
 def listFiles(url):
     cmd = 'duplicity list-current-files %s' % url
     stdoutdata = subprocess.getoutput(cmd)
@@ -32,12 +40,16 @@ def listFiles(url):
 
 def backup(url, fullpath, pw):
     os.environ['PASSPHRASE'] = pw
-    cmd = 'duplicity %s %s --progress --volsize 10' % (fullpath, url)
-    status, stdoutdata = subprocess.getstatusoutput(cmd)
+    cmd = 'duplicity %s %s --progress --volsize 25' % (fullpath, url)
+
+    l = None
+    for l in executeCmd(cmd):
+        print(l.strip())
+
     os.environ['PASSPHRASE'] = 'None'
-    if status != 0:
-        raise Exception('Status: %s' % status)
-    print(stdoutdata)
+    # Note sure this works as intended (l contains last return = status code)
+    if l != 0:
+        raise Exception('Status: %s' % l)
 
 if __name__ == '__main__':
     args = getArgs()
