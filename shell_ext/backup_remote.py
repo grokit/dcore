f4904f27ea521527a397b218a8a2f682d4ff71f1
d-asus <you@example.com>
2018-01-10 01:08:10 -0800

    gitpp: auto commit

--- shell_ext/backup_remote.py
+++ shell_ext/backup_remote.py
@@ -1,6 +1,5 @@
 """
 Backups to a remote location.
-Uses duplicity + backblaze b2 for storage.
 
 # TODO
 """
@@ -34,22 +33,10 @@ def executeCmd(cmd):
     return p.wait()
 
 def listFiles(url):
-    cmd = 'duplicity list-current-files %s' % url
+    cmd = 'ls'
     stdoutdata = subprocess.getoutput(cmd)
     print(stdoutdata)
 
-def backup(url, fullpath, pw):
-    os.environ['PASSPHRASE'] = pw
-    cmd = 'duplicity %s %s --progress --volsize 25' % (fullpath, url)
-
-    l = None
-    for l in executeCmd(cmd):
-        print(l.strip())
-
-    os.environ['PASSPHRASE'] = 'None'
-    # Note sure this works as intended (l contains last return = status code)
-    if l != 0:
-        raise Exception('Status: %s' % l)
 
 if __name__ == '__main__':
     args = getArgs()
