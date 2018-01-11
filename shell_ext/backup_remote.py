273bbb6fb4a9d7a25b14f08780c2186974a6e4c1
d-asus <you@example.com>
2018-01-11 01:04:37 -0800

    gitpp: auto commit

--- shell_ext/backup_remote.py
+++ shell_ext/backup_remote.py
@@ -18,6 +18,9 @@ _meta_shell_command = 'backup_remote'
 
 def getArgs():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--ls', action="store_true")
+    parser.add_argument('-m', '--mount', action="store_true")
+    parser.add_argument('-u', '--umount', action="store_true")
     args = parser.parse_args()
     return args
 
@@ -25,6 +28,7 @@ def getBackupPWAndUrl():
     return private_data.k_remote_backup_backblaze_v1, private_data.backblaze_b2_url
 
 def executeCmd(cmd):
+    print('Executing: ', cmd)
     cmd = cmd.split(' ')
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
     for l in p.stdout:
@@ -33,52 +37,70 @@ def executeCmd(cmd):
     return p.wait()
 
 def listSnapshots(url):
-    os.environ['BORG_PASSPHRASE'] = pw
     cmd = 'borg list %s --short' % (url)
     for l in executeCmd(cmd):
         print(l)
-    os.environ['BORG_PASSPHRASE'] = 'none'
 
 def getLastSnapshot(url):
-    os.environ['BORG_PASSPHRASE'] = pw
     cmd = 'borg list %s --short' % (url)
     last = None
     for l in executeCmd(cmd):
         last = l.strip()
-    os.environ['BORG_PASSPHRASE'] = 'none'
 
     if last is None:
         raise Exception()
     return last
 
 def listFiles(url, snapshot):
-    os.environ['BORG_PASSPHRASE'] = pw
     cmd = 'borg list %s::%s' % (url, snapshot)
     for l in executeCmd(cmd):
         print(l.strip())
-    os.environ['BORG_PASSPHRASE'] = 'none'
 
 def init(pw, url):
-    os.environ['BORG_PASSPHRASE'] = pw
     cmd = 'borg init %s' % (url)
     for l in executeCmd(cmd):
         print(l.strip())
-    os.environ['BORG_PASSPHRASE'] = 'none'
+
+def mount(pw, url):
+    snapshot = getLastSnapshot(url)
+    cmd = 'borg mount %s::%s /media/borg' % (url, snapshot)
+    for l in executeCmd(cmd):
+        print(l.strip())
+
+def umount(pw, url):
+    cmd = 'borg umount %s /media/borg' % (url)
+    for l in executeCmd(cmd):
+        print(l.strip())
 
 def backup(pw, url, pathToBackup):
-    os.environ['BORG_PASSPHRASE'] = pw
     pathToBackup = os.path.abspath(os.path.expanduser(pathToBackup))
     cmd = "borg create %s::Test-%s %s --stats --progress" % (url, int(1000*time.time()), pathToBackup)
     for l in executeCmd(cmd):
         print(l.strip())
-    os.environ['BORG_PASSPHRASE'] = 'none'
+
+def default():
+    pw, url = getBackupPWAndUrl()
+    backup(pw, url, '~/sync/dev/2dgame')
 
 if __name__ == '__main__':
     args = getArgs()
     pw, url = getBackupPWAndUrl()
 
-    #init(pw, url)
-    backup(pw, url, '~/sync/dev/coding_practice')
-    snapshot = getLastSnapshot(url)
-    listFiles(url, snapshot)
+    os.environ['BORG_PASSPHRASE'] = pw
+    try:
+        if args.ls:
+            snapshot = getLastSnapshot(url)
+            listFiles(url, snapshot)
+        elif args.mount:
+            mount(pw, url)
+        elif args.umount:
+            umount(pw, url)
+        else:
+            default()
+    except Exception as e:
+        os.environ['BORG_PASSPHRASE'] = 'none'
+        print(e)
+    os.environ['BORG_PASSPHRASE'] = 'none'
+
+
 
