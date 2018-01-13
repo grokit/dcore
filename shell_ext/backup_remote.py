de5b12d01becbb8024b272f81e1a670c760ce32b
d-asus <you@example.com>
2018-01-12 23:07:34 -0800

    gitpp: auto commit

--- shell_ext/backup_remote.py
+++ shell_ext/backup_remote.py
@@ -10,15 +10,18 @@ import time
 import argparse
 import getpass
 import subprocess
+import datetime
 
 import dcore.files as files
 import dcore.private_data as private_data
+import dcore.apps.gmail.gmail as gmail
 
 _meta_shell_command = 'backup_remote'
 
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--init', action="store_true")
+    parser.add_argument('-s', '--snapshots_list', action="store_true")
     parser.add_argument('-l', '--ls', action="store_true")
     parser.add_argument('-m', '--mount', action="store_true")
     parser.add_argument('-u', '--umount', action="store_true")
@@ -28,19 +31,26 @@ def getArgs():
 def getBackupPWAndUrl():
     return private_data.k_remote_backup_backblaze_v1, private_data.backblaze_b2_url
 
+def dateForAnnotation():
+    #int(1000*time.time())
+    #return datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")
+    return datetime.datetime.now().isoformat()
+
 def executeCmd(cmd):
-    print('Executing: ', cmd)
-    cmd = cmd.split(' ')
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    for l in p.stdout:
-        yield l
-    p.stdout.close()
-    return p.wait()
+    print('Executing: %s.' % cmd)
+
+    if False:
+        cmd = cmd.split(' ')
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        for l in p.stdout:
+            yield l
+        p.stdout.close()
+        return p.wait()
 
 def listSnapshots(url):
     cmd = 'borg list %s --short' % (url)
     for l in executeCmd(cmd):
-        print(l)
+        print(l.strip())
 
 def getLastSnapshot(url):
     cmd = 'borg list %s --short' % (url)
@@ -76,13 +86,21 @@ def umount(pw, url):
 
 def backup(pw, url, pathToBackup):
     pathToBackup = os.path.abspath(os.path.expanduser(pathToBackup))
-    cmd = "borg create %s::Test-%s %s --stats --progress" % (url, int(1000*time.time()), pathToBackup)
+    cmd = "borg create %s::AutoBackup-%s %s --stats --progress" % (url, dateForAnnotation(), pathToBackup)
+    L = []
     for l in executeCmd(cmd):
         print(l.strip())
+        L.append(l)
+    return "\n".join(L)
 
+def sendMail(content):
+    args = getArgs()
+    gmail.sendEmail(private_data.primary_email, "Backup Report m3pzBxlKu", content)
+    
 def default():
     pw, url = getBackupPWAndUrl()
-    backup(pw, url, '~/sync')
+    stdout = backup(pw, url, '~/sync')
+    sendMail(stdout)
 
 if __name__ == '__main__':
     args = getArgs()
@@ -93,6 +111,8 @@ if __name__ == '__main__':
         if args.ls:
             snapshot = getLastSnapshot(url)
             listFiles(url, snapshot)
+        elif args.snapshots_list:
+            listSnapshots(url)
         elif args.init:
             init(pw, url)
         elif args.mount:
