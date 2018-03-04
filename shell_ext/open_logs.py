7386533ccd3d4a04de8eacef4d2d127b0920e636
d-asus <you@example.com>
2018-03-04 09:56:36 -0800

    gitpp: auto commit

---
+++ shell_ext/open_logs.py
@@ -0,0 +1,31 @@
+﻿"""
+"""
+
+import os
+import argparse
+
+import dcore.data as data
+
+_meta_shell_command = 'logs'
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+    
+if __name__ == '__main__':
+    
+    args = getArgs()
+    folder = data.logsdir()
+    print('Using folder: %s.' % folder)
+
+    files = os.listdir(folder)
+    files = [f for f in files if os.path.isfile(f)]
+    files.sort(key=lambda x: os.path.getmtime(x))
+
+    if len(files) == 0:
+        print('No log file found.')
+    else:
+        file = os.path.abspath(files[0])
+        cmd = 'vi %s' % file
+        print(cmd)
+        os.system(cmd)
