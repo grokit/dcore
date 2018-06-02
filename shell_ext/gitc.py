b890cbee2bf118b52b5bfb39b948a7677f8f9950
d-asus <you@example.com>
2018-06-02 13:28:59 -0700

    add git c

---
+++ shell_ext/gitc.py
@@ -0,0 +1,20 @@
+
+_meta_shell_command = 'gitc'
+
+import os
+import sys
+
+
+if __name__ == '__main__':
+
+    assert len(sys.argv) > 1
+    
+    comment = " ".join(sys.argv[1:])
+
+    cmds = []
+    cmds.append('git add --all :/')
+    cmds.append('git commit -a -m "%s"' % comment)
+
+    for c in cmds:
+        print(c)
+        os.system(c)
