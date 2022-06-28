704b9aa5fa392f76db18364479514a8780a01024
grok_it <davidtgaulin@gmail.com>
2022-06-27 22:31:06 -0700

    hgr dedup

--- shell_ext/hgr.py
+++ shell_ext/hgr.py
@@ -18,13 +18,29 @@ def getArgs():
     return args
 
 if __name__ == '__main__':
-    args = getArgs()
-    regex = " ".join(args.regex).strip()
+    if len(sys.argv) > 1:
+        args = getArgs()
+        regex = " ".join(args.regex).strip()
+    else:
+        regex = '.*'
 
+    lst = []
+    lst_filtered = []
+    already_seen = set()
     with open(os.path.expanduser('~/.bash_history'), 'r') as fh:
         for line in fh.readlines():
             line = line.strip()
             mm = re.search(regex, line)
             if mm is not None:
-                print(line)
+                #print(line)
                 #print(mm.group(0))
+                lst.append(line)
+
+    for i in range(len(lst)-1, -1, -1):
+        if lst[i] not in already_seen:
+            lst_filtered.append(lst[i])
+            already_seen.add(lst[i])
+    lst_filtered.reverse()
+
+    for ll in lst_filtered:
+        print(ll)
