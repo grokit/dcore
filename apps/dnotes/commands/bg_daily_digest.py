bc09c1d3a153ff68d5b0f230ca6d074391461ab8
grok_it <davidtgaulin@gmail.com>
2024-09-06 19:13:22 -0700

    bg task as lib

--- apps/dnotes/commands/bg_daily_digest.py
+++ apps/dnotes/commands/bg_daily_digest.py
@@ -20,9 +20,9 @@ import dcore.apps.dnotes.score as score
 import dcore.apps.dnotes.search as search
 import dcore.apps.dnotes.util as util
 import dcore.apps.dnotes.options as options
+import dcore.apps.dnotes.bg_tasks as bg_tasks
 
 _meta_shell_command = 'bg_daily_digest'
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -33,36 +33,7 @@ def get_args():
     return parser.parse_args()
 
 
-def do():
-    today_str = datetime.datetime.today().strftime('%Y-%m-%d')
-    folder_out = os.path.join(dcore_data.dcoreTempData(), f'dnote_daily_digest', f'{today_str}')
-    if os.path.exists(folder_out):
-        return
-    print(f'daily digest not present, triggering in {folder_out}')
-    os.makedirs(folder_out)
-
-    files = util.get_all_note_files()
-
-    filename_out = os.path.join(folder_out, 'all_files.json')
-    with open(filename_out, 'w') as fh:
-        fh.write(json.dumps(files, indent=4))
-
-    filename_out = os.path.join(folder_out, 'all_metas.json')
-    meta_s = []
-    uuid_s = []
-    for ff in files:
-        metas = meta.extract(ff, open(ff, 'r').read())
-        for mm in metas:
-            meta_s.append((mm.meta_type, mm.value, mm.source_filename))
-            if mm.meta_type == 'uuid':
-                uuid_s.append((mm.source_filename, mm.value))
-    with open(filename_out, 'w') as fh:
-        fh.write(json.dumps(meta_s, indent=4))
-
-    filename_out = os.path.join(folder_out, 'all_uuids.json')
-    with open(filename_out, 'w') as fh:
-        fh.write(json.dumps(uuid_s, indent=4))
-
 if __name__ == '__main__':
     G_ARGS = get_args()
-    do()
+    bg_tasks.daily_digest()
+
